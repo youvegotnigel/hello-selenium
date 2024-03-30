95426543fc21c8f7e508253d69b22b5b85061518
Nigel <nigeldave19@gmail.com>
2024-03-31 00:29:54 +0530

    Update BaseTest.java

--- src/test/java/org/nigel/selenium/BaseTest.java
+++ src/test/java/org/nigel/selenium/BaseTest.java
@@ -3,25 +3,52 @@ package org.nigel.selenium;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.grid.Main;
 import org.openqa.selenium.net.PortProber;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.annotations.AfterTest;
 
 import java.net.URL;
 import java.time.Duration;
+import java.util.Arrays;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class BaseTest {
 
     protected WebDriver driver;
     protected WebDriverWait wait;
-    protected File driverPath;
-    protected File browserPath;
+//    protected File driverPath;
+//    protected File browserPath;
 
 
     public WebElement getLocatedElement(WebDriver driver, By by) {
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
         return wait.until(d -> driver.findElement(by));
+    }
+
+    protected FirefoxDriver startFirefoxDriver() {
+        return startFirefoxDriver(new FirefoxOptions());
+    }
+
+    protected FirefoxDriver startFirefoxDriver(FirefoxOptions options) {
+        options.setImplicitWaitTimeout(Duration.ofSeconds(1));
+        driver = new FirefoxDriver(options);
+        return (FirefoxDriver) driver;
+    }
+
+    protected ChromeDriver startChromeDriver() {
+        ChromeOptions options = new ChromeOptions();
+        options.setImplicitWaitTimeout(Duration.ofSeconds(1));
+        return startChromeDriver(options);
+    }
+
+    protected ChromeDriver startChromeDriver(ChromeOptions options) {
+        driver = new ChromeDriver(options);
+        return (ChromeDriver) driver;
     }
 
     protected URL startStandaloneGrid() {
@@ -45,8 +72,15 @@ public class BaseTest {
         }
     }
 
-    @AfterTest
-    public void quit() {
+    protected void enableLogging() {
+        Logger logger = Logger.getLogger("");
+        logger.setLevel(Level.FINE);
+        Arrays.stream(logger.getHandlers()).forEach(handler -> {
+            handler.setLevel(Level.FINE);
+        });
+    }
+
+    public void tearDown() {
         if (driver != null) {
             driver.quit();
         }
