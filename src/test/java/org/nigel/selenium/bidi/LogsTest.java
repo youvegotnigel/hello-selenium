303897375c495f470f31aea23e3bf72506d18571
Nigel <nigeldave19@gmail.com>
2024-07-14 17:53:16 +0530

    Update LogsTest.java

--- src/test/java/org/nigel/selenium/bidi/LogsTest.java
+++ src/test/java/org/nigel/selenium/bidi/LogsTest.java
@@ -3,6 +3,7 @@ package org.nigel.selenium.bidi;
 import org.nigel.selenium.BaseTest;
 import org.openqa.selenium.By;
 import org.openqa.selenium.bidi.log.ConsoleLogEntry;
+import org.openqa.selenium.bidi.log.JavascriptLogEntry;
 import org.openqa.selenium.bidi.module.LogInspector;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -62,14 +63,14 @@ public class LogsTest extends BaseTest {
 
         try (LogInspector logInspector = new LogInspector(driver)) {
 
-            CompletableFuture<ConsoleLogEntry> future = new CompletableFuture<>();
-            logInspector.onConsoleEntry(future::complete);
+            CompletableFuture<JavascriptLogEntry> future = new CompletableFuture<>();
+            logInspector.onJavaScriptLog(future::complete);
 
             driver.get("https://www.selenium.dev/selenium/web/bidi/logEntryAdded.html");
             driver.manage().window().maximize();
             driver.findElement(By.id("jsException")).click();
 
-            ConsoleLogEntry logEntry = future.get(5, TimeUnit.SECONDS);
+            JavascriptLogEntry logEntry = future.get(5, TimeUnit.SECONDS);
             System.out.println(logEntry.getText());
         }
     }
@@ -79,14 +80,14 @@ public class LogsTest extends BaseTest {
 
         try (LogInspector logInspector = new LogInspector(driver)) {
 
-            CompletableFuture<ConsoleLogEntry> future = new CompletableFuture<>();
-            logInspector.onConsoleEntry(future::complete);
+            CompletableFuture<JavascriptLogEntry> future = new CompletableFuture<>();
+            logInspector.onJavaScriptException(future::complete);
 
             driver.get("https://www.selenium.dev/selenium/web/bidi/logEntryAdded.html");
             driver.manage().window().maximize();
             driver.findElement(By.id("logWithStacktrace")).click();
 
-            ConsoleLogEntry logEntry = future.get(5, TimeUnit.SECONDS);
+            JavascriptLogEntry logEntry = future.get(5, TimeUnit.SECONDS);
             System.out.println(logEntry.getText());
         }
     }
